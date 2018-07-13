bd74c95abc3f7f95295c17fbcfe0e57036ce487b
Mike Hodges <rmikehodges@gmail.com>
2018-07-13 15:16:57 -0400

    added terraform module for google cloud functions

---
+++ terraform/modules/gcf-deployment/main.tf
@@ -0,0 +1,53 @@
+provider "google" {
+  credentials = "${file(var.google_credentials_path)}"
+  project     = "${var.gcp_project}"
+}
+
+resource "random_string" "bucket_name" {
+  length  = 8
+  special = false
+  number  = false
+  upper   = false
+}
+
+resource "google_storage_bucket" "bucket" {
+  name = "hidensneak${random_string.bucket_name.result}"
+
+  force_destroy = true
+}
+
+data "archive_file" "http_trigger" {
+  type        = "zip"
+  output_path = "${path.module}/files/redirector.zip"
+
+  source {
+    content  = "${file(var.redirector_file)}"
+    filename = "index.js"
+  }
+
+  source {
+    content  = "${file(var.package_file)}"
+    filename = "package.json"
+  }
+}
+
+resource "google_storage_bucket_object" "archive" {
+  name       = "redirector.zip"
+  bucket     = "${google_storage_bucket.bucket.name}"
+  source     = "${path.module}/files/redirector.zip"
+  depends_on = ["data.archive_file.http_trigger"]
+}
+
+resource "google_cloudfunctions_function" "hidensneak" {
+  name                  = "${var.function_name}"
+  entry_point           = "redirector"
+  available_memory_mb   = 128
+  timeout               = 61
+  project               = "${var.gcp_project}"
+  region                = "${var.region}"
+  trigger_http          = true
+  source_archive_bucket = "${google_storage_bucket.bucket.name}"
+  source_archive_object = "${google_storage_bucket_object.archive.name}"
+
+  depends_on = ["google_storage_bucket_object.archive"]
+}
---
+++ terraform/modules/gcf-deployment/variables.tf
@@ -0,0 +1,11 @@
+variable package_file {}
+
+variable redirector_file {}
+
+variable function_name {}
+
+variable region {}
+
+variable gcp_project {}
+
+variable "google_credentials_path" {}
